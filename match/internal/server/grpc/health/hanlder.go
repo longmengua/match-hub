35b16e7cc8948f2127c055568fd9ea96553962dd
Waltor <longmengua@users.noreply.github.com>
2025-07-11 14:48:02 +0800

    feat: update

--- match/internal/server/grpc/health/hanlder.go
+++ match/internal/server/grpc/health/hanlder.go
@@ -2,9 +2,6 @@ package health
 
 import (
 	"context"
-	"fmt"
-	"match/internal/enum"
-	"match/internal/errors"
 	"match/proto"
 )
 
@@ -20,7 +17,7 @@ func NewServer() *Server {
 func (s *Server) Check(ctx context.Context, req *proto.HealthRequest) (*proto.HealthResponse, error) {
 	version := "v1.0.0" // Replace with actual version retrieval logic, e.g., from build info or environment variable
 	if len(req.MustBeHello) == 0 {
-		return nil, errors.New(enum.InvalidParams, fmt.Errorf("Invalid request params"))
+		return &proto.HealthResponse{Version: version}, nil
 	}
 	return &proto.HealthResponse{Version: version}, nil
 }
